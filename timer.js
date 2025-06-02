b25dc76aa44e4ff614b3a81c0067386d75e4f1ad
Mohammad Bablu YT (ALI AKBAR) <91384223+mohammadbabluyt@users.noreply.github.com>
2025-06-02 11:21:50 +0600

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,23 @@
+// Timer Countdown Function
+const countDownDate = new Date("June 5, 2025 21:00:00").getTime();
+
+const countdownInterval = setInterval(function () {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  if (distance < 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("countdownTimer").innerHTML = "Match Started!";
+    return;
+  }
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days.toString().padStart(2, '0');
+  document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+  document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+  document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+}, 1000);
