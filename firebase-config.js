2f3adbd19146a094c658abf5827aa42472cab6b7
Mohammad Bablu YT (ALI AKBAR) <91384223+mohammadbabluyt@users.noreply.github.com>
2025-06-02 09:48:48 +0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+
+// আপনার Firebase প্রকল্পের তথ্য দিয়ে config
+const firebaseConfig = {
+  apiKey: "AIzaSyCetbTORukRM_CclT2iRbIM8zyeBDBXR1M",
+  authDomain: "taskforce-79e5a.firebaseapp.com",
+  databaseURL: "https://taskforce-79e5a-default-rtdb.firebaseio.com",
+  projectId: "taskforce-79e5a",
+  storageBucket: "taskforce-79e5a.appspot.com",
+  messagingSenderId: "240114611066",
+  appId: "1:240114611066:web:d00f2a67e2b8d7001b5cc3"
+};
+
+// Firebase ইনিশিয়ালাইজ
+firebase.initializeApp(firebaseConfig);
